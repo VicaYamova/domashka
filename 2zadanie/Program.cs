40d18cefb2b6aecfecaeb1fd3a3ee10c0f91a2e2
Vika <vica_che@mail.ru>
2022-08-24 02:51:09 +0300

    2zadanie

---
+++ 2zadanie/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Введите число ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+string userNumberText = Convert.ToString(userNumber);
+
+if(userNumberText.Length > 2)
+
+{
+    Console.WriteLine(userNumberText[2]);
+
+}
+
+else
+{
+Console.WriteLine("Третьей цифры нет");
+}
