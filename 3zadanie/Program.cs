12d617bc02c8b8b1d1a286fc142f507d915e43eb
Vika <vica_che@mail.ru>
2022-08-28 23:40:01 +0300

    3zadanie

---
+++ 3zadanie/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine
+("Введите цифру обозначающую день недели: 1 -  понедельник, 2 - вторник. 3 - среда,4 - четверг, 5 - пятница, 6 - суббота, 7 - воскресенье");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number >= 6)
+{     
+  Console.WriteLine("Это выходной день");
+}
+
+if(number < 6)
+{
+  Console.WriteLine("Это рабочий день");      
+}
